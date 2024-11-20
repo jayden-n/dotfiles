80ebbdaa1e119d7055fa6fe678f7d7fff7423817
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2024-11-19 23:09:12 -0700

    wip

--- .config/nvim/lua/mvim/plugins/colorscheme.lua
+++ .config/nvim/lua/mvim/plugins/colorscheme.lua
@@ -9,14 +9,9 @@ local M = {
       flavour = Mo.C.transparent and "mocha" or "macchiato",
       term_colors = true,
       transparent_background = Mo.C.transparent,
-      -- dim_inactive = {
-      --   enabled = false, -- dims the background color of inactive window
-      --   shade = "dark",
-      --   percentage = 0.15, -- percentage of the shade to apply to the inactive window
-      -- },
       styles = {
-        -- keywords = { "italic" },
-        functions = { "italic" },
+        keywords = { "italic" },
+        -- functions = { "italic" },
       },
       integrations = {
         harpoon = true,
